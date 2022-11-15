ca5a42daab924c9570ef1445511dc579c6a70739
lethaian2904 <lethaian884@gmail.com>
2022-11-15 01:45:57 -0800

    đăng ký,đăng nhập

---
+++ quanLyTiemChung/Login.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+//using System.Data;
+
+namespace quanLyTiemChung
+{
+    public partial class Login : Form
+    {
+        public Login()
+        {
+            InitializeComponent();
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDangnhap_Click(object sender, EventArgs e)
+        {
+            SqlConnection conn =  new SqlConnection(@"Data Source=LAPTOP-PSQC0VQB\SQLEXPRESS;Initial Catalog=quanlytiemchung;Integrated Security=True");
+            try
+            {
+                conn.Open();
+                string tk = txtTaikhoan.Text;
+                string mk = txtMatkhau.Text;
+                string sql = "select *from dangnhap where Tendangnhap='" + tk + "'and Matkhau='" + mk + "'";
+                SqlCommand cmd = new SqlCommand(sql, conn);
+                SqlDataReader dta = cmd.ExecuteReader();
+                if(dta.Read()==true)
+                {
+                    MessageBox.Show("Đăng nhập thành công","Thông báo",MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Đăng nhập thất bại", "Thông báo", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                }
+
+            }
+            catch
+            {
+                MessageBox.Show("Lỗi kết nối");
+            }
+        }
+
+        private void btnThoat_Click(object sender, EventArgs e)
+        {
+            DialogResult tb = MessageBox.Show("Bạn có muốn thoát hay không?", "Thông báo", MessageBoxButtons.OKCancel, MessageBoxIcon.Question);
+            if (tb == DialogResult.OK) ;
+            Application.Exit();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            dangky dangky = new dangky();
+            dangky.ShowDialog();
+        }
+    }
+}
---
+++ quanLyTiemChung/TaiKhoan.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace quanLyTiemChung
+{
+    class TaiKhoan
+    {
+        private string tendangnhap;
+        private string matkhau;
+
+        public TaiKhoan()
+        {
+
+        }
+
+        public TaiKhoan(string tendangnhap,string matkhau)
+        {
+            this.tendangnhap = tendangnhap;
+            this.matkhau = matkhau;
+        }
+
+        public string Tendangnhap { get => tendangnhap; set => tendangnhap = value; }
+        public string Matkhau { get => matkhau; set => matkhau = value; }
+    }
+}
