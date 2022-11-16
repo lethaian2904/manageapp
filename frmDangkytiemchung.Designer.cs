4107d12a154370e627106bce87e11677c68ff196
QuangHieu1502 <113867414+QuangHieu1502@users.noreply.github.com>
2022-11-15 22:35:33 -0800

    Add files via upload

---
+++ frmDangkytiemchung.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace Apptiemchung
+{
+    partial class frmDangkytiemchung
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCCCD = new System.Windows.Forms.TextBox();
+            this.txtMadinhdanh = new System.Windows.Forms.TextBox();
+            this.txtHovarten = new System.Windows.Forms.TextBox();
+            this.btnDangky = new System.Windows.Forms.Button();
+            this.dtpNgaysinh = new System.Windows.Forms.DateTimePicker();
+            this.cboGioitinh = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(133, 111);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CCCD";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(133, 175);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(154, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Mã định danh trẻ em";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(133, 249);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Họ và tên";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(133, 320);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Ngày sinh";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(133, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(67, 20);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Giới tính";
+            // 
+            // txtCCCD
+            // 
+            this.txtCCCD.Location = new System.Drawing.Point(334, 108);
+            this.txtCCCD.Name = "txtCCCD";
+            this.txtCCCD.Size = new System.Drawing.Size(285, 26);
+            this.txtCCCD.TabIndex = 5;
+            // 
+            // txtMadinhdanh
+            // 
+            this.txtMadinhdanh.Location = new System.Drawing.Point(334, 172);
+            this.txtMadinhdanh.Name = "txtMadinhdanh";
+            this.txtMadinhdanh.Size = new System.Drawing.Size(285, 26);
+            this.txtMadinhdanh.TabIndex = 6;
+            // 
+            // txtHovarten
+            // 
+            this.txtHovarten.Location = new System.Drawing.Point(334, 246);
+            this.txtHovarten.Name = "txtHovarten";
+            this.txtHovarten.Size = new System.Drawing.Size(285, 26);
+            this.txtHovarten.TabIndex = 7;
+            // 
+            // btnDangky
+            // 
+            this.btnDangky.Location = new System.Drawing.Point(334, 499);
+            this.btnDangky.Name = "btnDangky";
+            this.btnDangky.Size = new System.Drawing.Size(124, 33);
+            this.btnDangky.TabIndex = 10;
+            this.btnDangky.Text = "Đăng ký";
+            this.btnDangky.UseVisualStyleBackColor = true;
+            this.btnDangky.Click += new System.EventHandler(this.btnDangky_Click);
+            // 
+            // dtpNgaysinh
+            // 
+            this.dtpNgaysinh.Format = System.Windows.Forms.DateTimePickerFormat.Short;
+            this.dtpNgaysinh.Location = new System.Drawing.Point(334, 320);
+            this.dtpNgaysinh.Name = "dtpNgaysinh";
+            this.dtpNgaysinh.Size = new System.Drawing.Size(285, 26);
+            this.dtpNgaysinh.TabIndex = 11;
+            // 
+            // cboGioitinh
+            // 
+            this.cboGioitinh.FormattingEnabled = true;
+            this.cboGioitinh.Location = new System.Drawing.Point(334, 395);
+            this.cboGioitinh.Name = "cboGioitinh";
+            this.cboGioitinh.Size = new System.Drawing.Size(285, 28);
+            this.cboGioitinh.TabIndex = 12;
+            // 
+            // frmDangkytiemchung
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(796, 592);
+            this.Controls.Add(this.cboGioitinh);
+            this.Controls.Add(this.dtpNgaysinh);
+            this.Controls.Add(this.btnDangky);
+            this.Controls.Add(this.txtHovarten);
+            this.Controls.Add(this.txtMadinhdanh);
+            this.Controls.Add(this.txtCCCD);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "frmDangkytiemchung";
+            this.Text = "Đăng ký tiêm chủng";
+            this.Load += new System.EventHandler(this.frmDangkytiemchung_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCCCD;
+        private System.Windows.Forms.TextBox txtMadinhdanh;
+        private System.Windows.Forms.TextBox txtHovarten;
+        private System.Windows.Forms.Button btnDangky;
+        private System.Windows.Forms.DateTimePicker dtpNgaysinh;
+        private System.Windows.Forms.ComboBox cboGioitinh;
+    }
+}
+
---
+++ frmDangkytiemchung.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Apptiemchung
+{
+    public partial class frmDangkytiemchung : Form
+    {
+        List<string> listgt = new List<string>() { "Nam", "Nữ", "Khác" };
+        public static SqlConnection con;
+        public frmDangkytiemchung()
+        {
+            InitializeComponent();
+        }
+
+        Modify modify = new Modify();
+        private void btnDangky_Click(object sender, EventArgs e)
+        {
+            //SqlConnection conn = new SqlConnection(@"Data Source=DESKTOP-8OFGL8E\HIEU;Initial Catalog=Hethongtiemchung;Integrated Security=True");
+            
+                //conn.Open();
+            string cccd = txtCCCD.Text;
+                //string madntreem = txtMadinhdanh.Text;
+            string hovaten = txtHovarten.Text;
+            dtpNgaysinh.MinDate = DateTime.Now;
+            int dayofmonth = DateTime.DaysInMonth(DateTime.Now.Year, DateTime.Now.Month);
+            dtpNgaysinh.MaxDate = new DateTime(DateTime.Now.Year, DateTime.Now.Month, dayofmonth);
+            string ngaysinh = dtpNgaysinh.Value.ToShortDateString();
+            string gioitinh = cboGioitinh.Text;
+            
+            try
+            {
+                string sql = "Insert into Dangkytiemchung values('"+cccd+"' , '"+hovaten+"' , '"+ngaysinh+"' , '"+gioitinh+"')";
+                modify.Command(sql);
+            }
+            catch
+            {
+                MessageBox.Show("Đăng ký không thành công");
+            }
+
+
+        }
+
+        private void frmDangkytiemchung_Load(object sender, EventArgs e)
+        {
+            cboGioitinh.DataSource = listgt;
+        }
+    }
+}
