21b34561b617f2069e80928deef71be5030e3b83
lethaian2904 <lethaian884@gmail.com>
2022-12-03 19:15:24 +0700

    QLTC

---
+++ quanLyTiemChung/ThongTinVacxin.Designer.cs
@@ -0,0 +1,173 @@
+﻿
+namespace quanLyTiemChung
+{
+    partial class ThongTinVacxin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtTenvacxin = new System.Windows.Forms.TextBox();
+            this.txtChungloai = new System.Windows.Forms.TextBox();
+            this.txtQuocgiasanxuat = new System.Windows.Forms.TextBox();
+            this.txtThanhtien = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnDangky = new System.Windows.Forms.Button();
+            this.btnBack = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtTenvacxin
+            // 
+            this.txtTenvacxin.Location = new System.Drawing.Point(225, 105);
+            this.txtTenvacxin.Name = "txtTenvacxin";
+            this.txtTenvacxin.Size = new System.Drawing.Size(151, 22);
+            this.txtTenvacxin.TabIndex = 1;
+            // 
+            // txtChungloai
+            // 
+            this.txtChungloai.Location = new System.Drawing.Point(225, 188);
+            this.txtChungloai.Name = "txtChungloai";
+            this.txtChungloai.Size = new System.Drawing.Size(151, 22);
+            this.txtChungloai.TabIndex = 2;
+            // 
+            // txtQuocgiasanxuat
+            // 
+            this.txtQuocgiasanxuat.Location = new System.Drawing.Point(225, 253);
+            this.txtQuocgiasanxuat.Name = "txtQuocgiasanxuat";
+            this.txtQuocgiasanxuat.Size = new System.Drawing.Size(151, 22);
+            this.txtQuocgiasanxuat.TabIndex = 3;
+            // 
+            // txtThanhtien
+            // 
+            this.txtThanhtien.Location = new System.Drawing.Point(225, 323);
+            this.txtThanhtien.Name = "txtThanhtien";
+            this.txtThanhtien.Size = new System.Drawing.Size(151, 22);
+            this.txtThanhtien.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(55, 110);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(76, 17);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Tên vacxin";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(55, 191);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(75, 17);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Chủng loại";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(55, 258);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(126, 17);
+            this.label3.TabIndex = 8;
+            this.label3.Text = " Quốc gia sản xuất";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(55, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(76, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Thành tiền";
+            // 
+            // btnDangky
+            // 
+            this.btnDangky.Location = new System.Drawing.Point(156, 440);
+            this.btnDangky.Name = "btnDangky";
+            this.btnDangky.Size = new System.Drawing.Size(104, 44);
+            this.btnDangky.TabIndex = 11;
+            this.btnDangky.Text = "Đăng ký ";
+            this.btnDangky.UseVisualStyleBackColor = true;
+            // 
+            // btnBack
+            // 
+            this.btnBack.Location = new System.Drawing.Point(398, 441);
+            this.btnBack.Name = "btnBack";
+            this.btnBack.Size = new System.Drawing.Size(135, 43);
+            this.btnBack.TabIndex = 12;
+            this.btnBack.Text = "Quay lại";
+            this.btnBack.UseVisualStyleBackColor = true;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(193, 9);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(294, 32);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "THÔNG TIN VACXIN";
+            // 
+            // ThongTinVacxin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(749, 585);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnBack);
+            this.Controls.Add(this.btnDangky);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtThanhtien);
+            this.Controls.Add(this.txtQuocgiasanxuat);
+            this.Controls.Add(this.txtChungloai);
+            this.Controls.Add(this.txtTenvacxin);
+            this.Name = "ThongTinVacxin";
+            this.Text = "ThongTinVacxin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtTenvacxin;
+        private System.Windows.Forms.TextBox txtChungloai;
+        private System.Windows.Forms.TextBox txtQuocgiasanxuat;
+        private System.Windows.Forms.TextBox txtThanhtien;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnDangky;
+        private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label label5;
+    }
+}
